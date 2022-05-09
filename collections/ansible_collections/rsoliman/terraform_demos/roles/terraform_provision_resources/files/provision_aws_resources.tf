a71b79b3bc087990ca7162ac4daa3c22380ebb2f
raedrizk <raedrizk@gmail.com>
2022-05-09 15:36:25 -0400

    adding http ports to aws

--- collections/ansible_collections/rsoliman/terraform_demos/roles/terraform_provision_resources/files/provision_aws_resources.tf
+++ collections/ansible_collections/rsoliman/terraform_demos/roles/terraform_provision_resources/files/provision_aws_resources.tf
@@ -39,6 +39,20 @@ resource "aws_security_group" "Terraform_Demo_SG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port       = 0
     to_port         = 0
